25fdc8f38f45cd2cb4b8fafe2f429201df83e89d
Shih-Chiang (SC) Lin <shichung@hasfoolsay-lm.tpcity.corp.yahoo.com>
2016-12-21 18:01:00 +0800

    corrected configs and codes to resolve builds errors of the repository yahoo/parsec

--- parsec-gradle-plugin/src/test/groovy/com/yahoo/parsec/gradle/ParsecGenerateTest.groovy
+++ parsec-gradle-plugin/src/test/groovy/com/yahoo/parsec/gradle/ParsecGenerateTest.groovy
@@ -91,7 +91,7 @@ class ParsecGenerateTest extends Specification {
 
         where:
             handleUncaughtExceptions | generateParsecError | parsecErrorGenerated
-//            false                    | false               | false
+            false                    | false               | true
             true                     | false               | true
             false                    | true                | true
             true                     | true                | true
