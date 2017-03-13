4299ba28776f1b48ee3c8e2a089b417854ed9cad
Travis CI <builds@travis-ci.com>
2017-03-13 08:00:03 +0000

    [Gradle Release Plugin][ci skip] new version release: 'parsec-0.0.33-pre'.

--- parsec-template-plugin/installation/apply.groovy
+++ parsec-template-plugin/installation/apply.groovy
@@ -6,7 +6,7 @@ buildscript {
     }
   }
   dependencies {
-    classpath "com.yahoo.parsec:parsec-template-plugin:0.0.32-pre"
+    classpath "com.yahoo.parsec:parsec-template-plugin:0.0.33-pre"
   }
 }
 
